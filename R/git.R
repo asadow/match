d71ccd4d51fe362434451602cf3bf53d18af135c
Adam Sadowski <asebsadowski@gmail.com>
2022-12-06 12:45:59 -0500

    .

---
+++ R/git.R
@@ -0,0 +1,11 @@
+library(usethis)
+usethis::create_from_github("https://github.com/asadow/website.git",
+                            destdir = "~/Library/CloudStorage/OneDrive-UniversityofGuelph/match")
+
+usethis::use_git()
+
+gh_token_help()
+git_sitrep()
+
+
+git_vaccinate()
